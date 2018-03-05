a75fd72c1b9389b4e102c6cecad8d998204fb354
kartik <kartikanand1992@gmail.com>
2018-03-05 20:14:02 +0530

    way better with double spacing

--- imgart.py
+++ imgart.py
@@ -1,14 +1,14 @@
 from PIL import Image
 
 im = Image.open('sample.bmp')
-im = im.resize((16, 16))
+im = im.resize((32, 32))
 
 width, height = im.size
 
 for y in range(height):
 	for x in range(width):
-		d = im.getpixel((x, y))
-		print('{:2} '.format(d), end='')
-		#if im.getpixel((x, y)) < 15:
-		#	print('.', end='')
-	print('\n')
+		if im.getpixel((x, y)) < 15:
+			print('# ', end='')
+		else:
+			print('. ', end='')
+	print()
